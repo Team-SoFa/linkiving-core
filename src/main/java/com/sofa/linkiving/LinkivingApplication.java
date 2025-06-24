b7b410868c810d814ecd9d700d8c0141c0d02341
Jansoon <ckdals4600@naver.com>
2025-06-24 22:13:13 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/sofa/linkiving/LinkivingApplication.java
@@ -0,0 +1,13 @@
+package com.sofa.linkiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkivingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofa/linkiving/LinkivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofa.linkiving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
