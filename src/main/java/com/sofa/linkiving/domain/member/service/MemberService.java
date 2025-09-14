6bf18b17ec8c83c294fec84cb0cebb7f245268b9
Jansoon <ckdals4600@naver.com>
2025-09-14 21:25:58 +0900

    feat: 회원가입 서비스 로직 추가 (#47) - MemberService: 회원가입 핵심 비즈니스 로직 처리 - MemberCommandService: Member 저장 로직 처리

---
+++ src/main/java/com/sofa/linkiving/domain/member/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.sofa.linkiving.domain.member.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sofa.linkiving.domain.member.dto.request.SignupReq;
+import com.sofa.linkiving.domain.member.dto.response.MemberRes;
+import com.sofa.linkiving.domain.member.entity.Member;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+	private final MemberCommandService memberCommandService;
+
+	public MemberRes signup(SignupReq req) {
+		Member member = memberCommandService.addUser(req.email(), req.password());
+
+		return new MemberRes(member);
+	}
+}
