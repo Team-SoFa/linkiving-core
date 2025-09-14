93216c6f003de1007d8f496b0fb30154a2e86520
Jansoon <ckdals4600@naver.com>
2025-09-15 08:46:02 +0900

    refactor: NotBlank 변경 (#35)

--- src/main/java/com/sofa/linkiving/domain/member/entity/Member.java
+++ src/main/java/com/sofa/linkiving/domain/member/entity/Member.java
@@ -2,8 +2,8 @@ package com.sofa.linkiving.domain.member.entity;
 
 import com.sofa.linkiving.global.common.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,9 +13,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
-	@NotBlank
+
+	@Column(nullable = false)
 	private String email;
-	@NotBlank
+	@Column(nullable = false)
 	private String password;
 
 	@Builder
