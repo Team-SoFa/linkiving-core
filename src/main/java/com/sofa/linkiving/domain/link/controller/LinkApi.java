b45cd52a7ea7aea66f74c7edca67c59fd103d74d
minibr <minimin9823@gmail.com>
2025-10-12 17:27:57 +0900

    fix:  어노테이션 제거 (#53)

---
+++ src/main/java/com/sofa/linkiving/domain/link/controller/LinkApi.java
@@ -0,0 +1,9 @@
+package com.sofa.linkiving.domain.link.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Link", description = "링크 관리 API")
+public interface LinkApi {
+
+	// TODO: API 명세 추가 예정
+}
--- src/main/java/com/sofa/linkiving/domain/link/controller/LinkController.java
+++ src/main/java/com/sofa/linkiving/domain/link/controller/LinkController.java
@@ -1,14 +1,9 @@
 package com.sofa.linkiving.domain.link.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sofa.linkiving.domain.link.service.LinkService;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping("/api/links")
 @RequiredArgsConstructor
 public class LinkController {
 
