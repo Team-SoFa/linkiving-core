9ea31cf192f6983e28e1cf64c013c5952774aaec
Jansoon <57699212+ckdals4600@users.noreply.github.com>
2025-12-10 17:27:44 +0900

    feat: 테스트용 임시 로그인/회원가입 토큰 반환 구현 및 Security 설정 허용 (#116)

--- src/main/java/com/sofa/linkiving/domain/member/controller/MemberApi.java
+++ src/main/java/com/sofa/linkiving/domain/member/controller/MemberApi.java
@@ -2,7 +2,7 @@ package com.sofa.linkiving.domain.member.controller;
 
 import com.sofa.linkiving.domain.member.dto.request.LoginReq;
 import com.sofa.linkiving.domain.member.dto.request.SignupReq;
-import com.sofa.linkiving.domain.member.dto.response.MemberRes;
+import com.sofa.linkiving.domain.member.dto.response.TokenRes;
 import com.sofa.linkiving.global.common.BaseResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,8 +11,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "User")
 public interface MemberApi {
 	@Operation(summary = "회원가입", description = "이메일, 비밀번호를 통한 회원가입을 진행합니다.")
-	BaseResponse<MemberRes> signup(SignupReq req);
+	BaseResponse<TokenRes> signup(SignupReq req);
 
 	@Operation(summary = "로그인", description = "이메일, 비밀번호를 통해 로그인을 진행합니다.")
-	BaseResponse<MemberRes> login(LoginReq req);
+	BaseResponse<TokenRes> login(LoginReq req);
 }
