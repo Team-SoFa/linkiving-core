dd7f59a552dea4d2d5f536c350e221ad672e68fa
Jansoon <57699212+ckdals4600@users.noreply.github.com>
2025-12-10 17:27:44 +0900

    feat: 테스트용 임시 로그인/회원가입 토큰 반환 구현 및 Security 설정 허용 (#116)

--- src/main/java/com/sofa/linkiving/domain/member/controller/MemberController.java
+++ src/main/java/com/sofa/linkiving/domain/member/controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sofa.linkiving.domain.member.dto.request.LoginReq;
 import com.sofa.linkiving.domain.member.dto.request.SignupReq;
-import com.sofa.linkiving.domain.member.dto.response.MemberRes;
+import com.sofa.linkiving.domain.member.dto.response.TokenRes;
 import com.sofa.linkiving.domain.member.service.MemberService;
 import com.sofa.linkiving.global.common.BaseResponse;
 
@@ -23,16 +23,16 @@ public class MemberController implements MemberApi {
 
 	@Override
 	@PostMapping("/signup")
-	public BaseResponse<MemberRes> signup(@RequestBody @Validated SignupReq req) {
-		MemberRes signup = memberService.signup(req);
+	public BaseResponse<TokenRes> signup(@RequestBody @Validated SignupReq req) {
+		TokenRes signup = memberService.signup(req);
 
 		return BaseResponse.success(signup, "회원 가입에 성공하였습니다.");
 	}
 
 	@Override
 	@PostMapping("/login")
-	public BaseResponse<MemberRes> login(@Validated @RequestBody LoginReq req) {
-		MemberRes login = memberService.login(req);
+	public BaseResponse<TokenRes> login(@Validated @RequestBody LoginReq req) {
+		TokenRes login = memberService.login(req);
 
 		return BaseResponse.success(login, "로그인에 성공하였습니다.");
 	}
