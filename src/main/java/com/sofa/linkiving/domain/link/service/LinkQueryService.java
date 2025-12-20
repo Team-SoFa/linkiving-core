ded8c8e29527f87fdfaea8f967cc874a5dcfaac0
Jansoon <ckdals4600@naver.com>
2025-12-21 08:21:36 +0900

    refactor: 링크 조회 API 및 링크 목록 조회 API 응답 개선 (#157)

--- src/main/java/com/sofa/linkiving/domain/link/service/LinkQueryService.java
+++ src/main/java/com/sofa/linkiving/domain/link/service/LinkQueryService.java
@@ -1,11 +1,13 @@
 package com.sofa.linkiving.domain.link.service;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.sofa.linkiving.domain.link.dto.internal.LinkDto;
+import com.sofa.linkiving.domain.link.dto.internal.LinksDto;
 import com.sofa.linkiving.domain.link.entity.Link;
 import com.sofa.linkiving.domain.link.error.LinkErrorCode;
 import com.sofa.linkiving.domain.link.repository.LinkRepository;
@@ -28,8 +30,23 @@ public class LinkQueryService {
 			.orElseThrow(() -> new BusinessException(LinkErrorCode.LINK_NOT_FOUND));
 	}
 
-	public Page<Link> findAllByMember(Member member, Pageable pageable) {
-		return linkRepository.findByMemberAndIsDeleteFalse(member, pageable);
+	public LinkDto findByIdWithSummary(Long linkId, Member member) {
+		return linkRepository.findByIdAndMemberWithSummaryAndIsDeleteFalse(linkId, member)
+			.orElseThrow(() -> new BusinessException(LinkErrorCode.LINK_NOT_FOUND));
+	}
+
+	public LinksDto findAllByMemberWithSummaryAndCursor(Member member, Long lastId, int size) {
+		PageRequest pageRequest = PageRequest.of(0, size + 1);
+		List<LinkDto> linkDtos = linkRepository.findAllByMemberWithSummaryAndCursorAndIsDeleteFalse(member, lastId,
+			pageRequest);
+
+		boolean hasNext = false;
+		if (linkDtos.size() > size) {
+			hasNext = true;
+			linkDtos.remove(size);
+		}
+
+		return new LinksDto(linkDtos, hasNext);
 	}
 
 	public boolean existsByUrl(Member member, String url) {
