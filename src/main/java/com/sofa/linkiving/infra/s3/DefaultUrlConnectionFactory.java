7803bf93350d1ccd3c4ef1f1747deaba36fe2961
minibr <minimin9823@gmail.com>
2026-01-21 18:44:28 +0900

    feat: OG 이미지 캐싱 및 업로드 보호 (#169)

--- src/main/java/com/sofa/linkiving/infra/s3/DefaultUrlConnectionFactory.java
+++ src/main/java/com/sofa/linkiving/infra/s3/DefaultUrlConnectionFactory.java
@@ -1,6 +1,7 @@
 package com.sofa.linkiving.infra.s3;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -12,5 +13,13 @@ public class DefaultUrlConnectionFactory implements UrlConnectionFactory {
 	@Override
 	public URLConnection createConnection(String url) throws IOException {
 		return new URL(url).openConnection();
+	}
+
+	@Override
+	public InputStream openStream(String url) throws IOException {
+		URLConnection connection = createConnection(url);
+		connection.setConnectTimeout(5000);
+		connection.setReadTimeout(5000);
+		return connection.getInputStream();
 	}
 }
--- src/main/java/com/sofa/linkiving/infra/s3/UrlConnectionFactory.java
+++ src/main/java/com/sofa/linkiving/infra/s3/UrlConnectionFactory.java
@@ -1,8 +1,11 @@
 package com.sofa.linkiving.infra.s3;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URLConnection;
 
 public interface UrlConnectionFactory {
 	URLConnection createConnection(String url) throws IOException;
+
+	InputStream openStream(String url) throws IOException;
 }
