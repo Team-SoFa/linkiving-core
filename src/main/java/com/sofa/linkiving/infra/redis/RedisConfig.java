7bdfc74bdc4a48754fef720e8e2921acd805e48e
Goder-0 <kai.na@cluml.com>
2026-02-04 19:36:43 +0900

    fix: Enable SSL for Redis connection (#176)

--- src/main/java/com/sofa/linkiving/infra/redis/RedisConfig.java
+++ src/main/java/com/sofa/linkiving/infra/redis/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -23,9 +25,17 @@ public class RedisConfig {
 	@Value("${spring.data.redis.port}")
 	private int port;
 
+	@Value("${spring.data.redis.ssl.enabled:false}")
+	private boolean sslEnabled;
+
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+		LettuceClientConfiguration clientConfig = sslEnabled
+			? LettuceClientConfiguration.builder().useSsl().build()
+			: LettuceClientConfiguration.builder().build();
+
+		return new LettuceConnectionFactory(config, clientConfig);
 	}
 
 	@Bean
