82d2112146b17916e1a51a081297f76991c58ec7
Jansoon <ckdals4600@naver.com>
2026-04-30 19:49:29 +0900

    feat: 쿠키 기반 STOMP 웹소켓 인증 로직 추가 및 하이브리드 인증 구현 (#223)
    - [WebSocketConfig] HTTP Handshake 시 accessToken 쿠키를 웹소켓 세션으로 복사하는 HandshakeInterceptor 추가
    - [StompHandler] STOMP CONNECT 시 세션(쿠키) 토큰과 헤더 토큰을 모두 지원하도록 하이브리드 인증 로직 구현

--- src/main/java/com/sofa/linkiving/global/config/WebSocketConfig.java
+++ src/main/java/com/sofa/linkiving/global/config/WebSocketConfig.java
@@ -1,18 +1,25 @@
 package com.sofa.linkiving.global.config;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import com.sofa.linkiving.security.config.StompHandler;
 import com.sofa.linkiving.security.resolver.AuthMemberWebsocketArgumentResolver;
 
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -33,12 +40,38 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		HandshakeInterceptor cookieInterceptor = new HandshakeInterceptor() {
+			@Override
+			public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+				WebSocketHandler wsHandler, Map<String, Object> attributes) {
+				if (request instanceof ServletServerHttpRequest servletRequest) {
+					Cookie[] cookies = servletRequest.getServletRequest().getCookies();
+					if (cookies != null) {
+						for (Cookie cookie : cookies) {
+							if ("accessToken".equals(cookie.getName())) {
+								attributes.put("accessToken", cookie.getValue()); // 세션 주머니에 보관!
+								break;
+							}
+						}
+					}
+				}
+				return true;
+			}
+
+			@Override
+			public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+				WebSocketHandler wsHandler, Exception exception) {
+			}
+		};
+
 		registry.addEndpoint("/ws/chat")
 			.setAllowedOriginPatterns("*")
+			.addInterceptors(cookieInterceptor)
 			.withSockJS();
 
 		registry.addEndpoint("/ws/link")
 			.setAllowedOriginPatterns("*")
+			.addInterceptors(cookieInterceptor)
 			.withSockJS();
 	}
 
